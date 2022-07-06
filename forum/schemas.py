9e97d35014a7b6a4ad552af2c3e6d1cba7a08e8d
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-07-06 14:18:50 +0430

    add answers field to TopicResponse sechema

--- forum/schemas.py
+++ forum/schemas.py
@@ -1,11 +1,24 @@
+from typing import List
 from pydantic import BaseModel
 import datetime
+
+class AnswerResponse(BaseModel):
+    id: int
+    content: str
+    creator: str
+    votes: int
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+    class Config:
+        orm_mode = True
 
 class TopicResponse(BaseModel):
     id: int
     title: str
     content: str
     votes: int
+    answers: List[AnswerResponse]
     creator: str
     created_at: datetime.datetime
     updated_at: datetime.datetime
@@ -21,29 +34,16 @@ class TopicCreate(BaseModel):
     class Config:
         orm_mode = True
 
-class TopicUpdate(BaseModel):
-    title: str
-    content: str
-
-    class Config:
-        orm_mode = True
-
-
-class AnswerResponse(BaseModel):
-    id: int
-    content: str
-    creator: str
-    votes: int
-    created_at: datetime.datetime
-    updated_at: datetime.datetime
-
-    class Config:
-        orm_mode = True
-
-
 class AnswerCreate(BaseModel):
     content: str
     creator: str
+
+    class Config:
+        orm_mode = True
+
+class TopicUpdate(BaseModel):
+    title: str
+    content: str
 
     class Config:
         orm_mode = True
