1223344dfc95558b71e0d3b9dd2dc88dcdd48ba9
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-07-08 09:54:15 +0430

    create tables code in main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from forum.routes import routes as forum_routes
+from auth.routes import routes as auth_routes
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+from db_config import engine, Base
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -26,12 +30,12 @@ app.add_middleware(
 )
 
 app.include_router(forum_routes)
+app.include_router(auth_routes)
 
 @app.get("/favicon.ico")
-async def favicon():
+def favicon():
     return RedirectResponse("/static/favicon.png")
 
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8070)
+    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8070)
