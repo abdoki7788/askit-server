6c924b4fac7fff7e5706932bba9fae1ecb3935a0
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-07-10 15:30:07 +0430

    add validations to user model and handle errors in create_user crud function

--- auth/crud.py
+++ auth/crud.py
@@ -1,4 +1,6 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from auth import models, schemas, utils
 
 def get_user_by_username(db: Session, username: str):
@@ -8,8 +10,15 @@ def get_users(db: Session):
     return db.query(models.User).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(username=user.username, hashed_password=utils.get_password_hash(user.password))
-    db.add(db_user)
-    db.commit()
+    try:
+        db_user = models.User(username=user.username, hashed_password=utils.get_password_hash(user.password))
+        db.add(db_user)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(detail="User already exists", status_code=400)
+    except ValueError as e:
+        db.rollback()
+        raise HTTPException(detail=str(e), status_code=400)
     db.refresh(db_user)
     return db_user
--- auth/models.py
+++ auth/models.py
@@ -1,6 +1,7 @@
-from db_config import Base, engine
+from db_config import Base
+import re
 from sqlalchemy import Column, Integer, String, Boolean
-from . import models
+from sqlalchemy.orm import validates
 
 class User(Base):
     __tablename__ = 'users'
@@ -10,3 +11,21 @@ class User(Base):
     full_name = Column(String, nullable=True)
     disabled = Column(Boolean, default=False)
     hashed_password = Column(String, nullable=False)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if re.fullmatch(r"^[a-z0-9_-]{3,15}$", username) is None:
+            raise ValueError('Username must be at least 3 characters long and contain only letters, numbers, underscores, and dashes.')
+        return username
+    
+    @validates('full_name')
+    def validate_full_name(self, key, full_name):
+        if re.fullmatch(r"^[a-zA-Z ]{3,30}$", full_name) is None:
+            raise ValueError('Full name must be at least 3 characters long and contain only letters.')
+        return full_name
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if re.fullmatch(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email) is None:
+            raise ValueError('Email must be a valid email address.')
+        return email
