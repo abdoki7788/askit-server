db0c7a63618b449da3e746d1f7bb8755ea00fcdc
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-07-10 19:08:09 +0430

    use TopicListResponse in get_tag_topics route and change routes name

--- tags/routes.py
+++ tags/routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from db_config import get_db
 from . import crud, schemas
-from forum.schemas import TopicResponse
+from forum.schemas import TopicListResponse
 
 routes = APIRouter(prefix="/tags")
 
@@ -15,6 +15,6 @@ def get_tags(db: Session = Depends(get_db)):
 def get_tags(tag_id: int, db: Session = Depends(get_db)):
     return crud.get_tag(db=db, tag_id=tag_id)
 
-@routes.get("/{tag_id}/topics", status_code=200, response_model=List[TopicResponse])
+@routes.get("/{tag_id}/topics", status_code=200, response_model=List[TopicListResponse])
 def get_tags(tag_id: int, db: Session = Depends(get_db)):
     return crud.get_tag_topics(db=db, tag_id=tag_id)
