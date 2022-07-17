0a8274063bf5ef1739630ff18a67f20b8460e026
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-07-17 17:22:47 +0430

    pagination

--- forum/routes.py
+++ forum/routes.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from . import crud, schemas, utils
 from auth.dependencies import oauth2_scheme, get_current_active_user
 from auth.schemas import UserVoteResponse
+from fastapi_pagination import Page, Params, paginate
 from db_config import get_db
 
 routes = APIRouter(prefix="/topics", tags=["topics"])
@@ -12,9 +13,9 @@ routes = APIRouter(prefix="/topics", tags=["topics"])
 def create_topic(topic: schemas.TopicCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: int = Depends(get_current_active_user)):
     return crud.create_topic(db=db, topic=topic, user_id=current_user.id)
 
-@routes.get("", response_model=List[schemas.TopicListResponse], status_code=200)
-def topics(db: Session = Depends(get_db)):
-    return crud.get_topics(db=db)
+@routes.get("", response_model=Page[schemas.TopicListResponse], status_code=200)
+def topics(db: Session = Depends(get_db), params: Params = Depends()):
+    return paginate(crud.get_topics(db=db), params)
 
 @routes.get("/hot", response_model=List[schemas.HotTopics])
 def hot_topics(db: Session = Depends(get_db)):
@@ -62,7 +63,7 @@ def is_favorite_topic(topic_id: int, db: Session = Depends(get_db), token: str =
 def create_answer(topic_id: int, answer: schemas.AnswerCreate, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: int = Depends(get_current_active_user)):
     return crud.create_answer(db=db, answer=answer, topic_id=topic_id, creator_id=current_user.id)
 
-@routes.get("/{topic_id}/answers", response_model=List[schemas.AnswerResponse], status_code=200)
+@routes.get("/{topic_id}/answers", response_model=Page[schemas.AnswerResponse], status_code=200)
 def topic_answers(topic_id: int, db: Session = Depends(get_db)):
     return crud.get_answers(db=db, topic_id=topic_id)
 
